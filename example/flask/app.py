cda6c63403d200e44f7a574b1bb89eb3fea13b6d
Your Name <you@example.com>
2019-07-14 10:39:28 +0700

    Fix connection from Flask app to mariadb

--- example/flask/app.py
+++ example/flask/app.py
@@ -2,23 +2,21 @@ from flask import Flask, redirect, url_for, request, render_template, make_respo
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'XYZ'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/mysql'
+# app.secret_key = 'XYZ'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:donghm@127.0.0.1/automation'
 
 db = SQLAlchemy(app)
 
-class AddressBook(db.Model):
+class Human(db.Model):
     id = db.Column('id', db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    city = db.Column(db.String(50))
-    addr = db.Column(db.String(200))
-    pin = db.Column(db.String(10))
+    name = db.Column(db.String(255))
+    address = db.Column(db.String(255))
+    city = db.Column(db.String(255))
 
-    def __init__(self, name, city, addr, pin):
+    def __init__(self, name, address, city):
         self.name = name
+        self.address = address
         self.city = city
-        self.addr = addr
-        self.pin = pin
 
 @app.route('/address')
 def address():
@@ -28,14 +26,16 @@ def address():
 
 @app.route('/show_all')
 def show_all():
-    return render_template('show_all.html')
-    # return render_template('show_all.html', addresses = AddressBook.query.all())
+    # return render_template('show_all.html')
+    human = Human.query.all()
+    print(human)
+    return render_template('show_all.html', human = human)
 
 
 @app.route('/show_update', methods=['POST', 'GET'])
 def show_update():
     id = request.args.get('id')
-    address = AddressBook.query.filter_by(id=id).first()
+    human = AddressBook.query.filter_by(id=id).first()
     return render_template('update_addr.html', address=address)
 
 @app.route('/new_addr', methods=['POST', 'GET'])
@@ -45,10 +45,10 @@ def new_addr():
         print(request.form['address'])
         print(request.form['city'])
         name = request.form['name']
-        # address = AddressBook(request.form['name'], request.form['address'],request.form['city'])
-        # db.session.add(address)
-        # db.session.commit()
-    return render_template('show_all.html', name=name)
+        h = Human(request.form['name'], request.form['address'],request.form['city'])
+        db.session.add(h)
+        db.session.commit()
+    return render_template('show_all.html', human = Human.query.all())
     # return render_template('show_all.html', addresses = AddressBook.query.all())
 
 @app.route('/delete_addr')
